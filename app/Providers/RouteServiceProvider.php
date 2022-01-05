993101a0a0265434c41b03faef245497b7cefe71
Tom Kuijer <t.kuijer@ifresh.nl>
2022-01-05 15:50:49 +0100

    Larastan with configuration file

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,7 +19,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/';
 
-    /**
+    /*
      * The controller namespace for the application.
      *
      * When present, controller route declarations will automatically be prefixed with this namespace.
